1b80cc7f55aea8a70329a4f32216b17bcbd05df4
sreedhar <sreedharks2003@gmail.com>
2022-05-07 18:20:36 +0530

    from bash

--- App.js
+++ App.js
@@ -1,12 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import {Feather} from '@expo/vector-icons';
+import Home from './Components/screens/Home';
+import AddExpense from './Components/screens/AddExpense';
+
+const Tab =createBottomTabNavigator();
 export default function App() {
+
+
+ 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+       <NavigationContainer>
+        <Tab.Navigator screenOptions={{
+          tabBarShowLabel:false,
+          tabBarStyle:{backgroundColor:'#181818'}
+
+        }}>
+            <Tab.Screen component={Home}  name={'Home'} 
+             options={{
+               tabBarIcon:({focused})=>{
+                 return(
+                   <View>
+                      <Feather name='home' size={24} color={focused?'white':'B3B3B3'}/>
+                   </View>
+                 );
+               },
+               headerStyle:{backgroundColor:'black'},
+               headerTitleStyle:{color:'white'}
+             }}
+            />
+            <Tab.Screen component={AddExpense} name={'Add'} 
+             options={{
+               tabBarIcon:({focused})=>{
+                 return(
+                   <View>
+                      <Feather name='plus-circle' size={24} color={focused?'white':'B3B3B3'}/>
+                   </View>
+                 );
+               },
+               headerStyle:{backgroundColor:'black'},
+               headerTitleStyle:{color:'white'}
+             }}
+            />
+        </Tab.Navigator>
+       </NavigationContainer>
+    
   );
 }
 
